5576a0da3bdcab346df3992c08f34fcaf2671a41
AndreyTHT <andreythtand@gmail.com>
2026-08-02 14:36:50 +0400

    add rep and service

--- SQL/Services/ProductService.cs
+++ SQL/Services/ProductService.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Data;
 
 namespace SQL.Services
 {
     internal class ProductService
     {
+        private List<Product> products = new List<Product>();
+
+        public List<Product> GetProductListFromTable(DataTable table)
+        {
+            foreach (DataRow row in table.Rows)
+            {
+                Product product = new Product
+                (
+                    Id: (int)row["Id"],
+                    Article: (string?)row["Article"],
+                    Name: (string?)row["Name"],
+                    Image: (string?)row["Image"],
+                    CategoryId: (int?)row["CategoryId"],
+                    Price: (int?)row["Price"],
+                    Count: (int?)row["Count"]
+                );
+                products.Add(product);
+            }
+            return products;
+        }
     }
 }
