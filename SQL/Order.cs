af1d239b5cf26e0a63a94876c4d8a51fa10f3af3
AndreyTHT <andreythtand@gmail.com>
2026-08-04 11:10:02 +0400

    add order

---
+++ SQL/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace SQL
+{
+    internal record Order(
+        int Id,
+        string? UserId,
+        int? TotalPrice,
+        DateTime? OrderDate,
+        string? DeliveryType,
+        string? RecipientName,
+        string? RecipientPhone,
+        string? RecipientEmail,
+        string? Address,
+        string? Comment
+    );
+}
---
+++ SQL/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SQL.Repositories
+{
+    internal class OrderRepository
+    {
+        private readonly SqlExecutor db;
+
+        public OrderRepository(SqlExecutor db)
+        {
+            this.db = db;
+        }
+
+        public async Task<DataTable> GetAllOrders()
+        {
+            string sql = "SELECT * FROM \"Orders\"";
+            return await db.SelectAsync(sql);
+        }
+    }
+}
